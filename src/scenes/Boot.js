733df348ecd9f329511b03a8280844defca3acf3
CorncobbStudios <cydonia15@gmail.com>
2026-07-02 17:01:42 +0000

    deleted preloader background image that didn't exist

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -4,11 +4,6 @@ export class Boot extends Phaser.Scene {
     constructor() {
         super('Boot');
     }
-
-    preload() {
-        this.load.image('preloaderBackground', '/assets/bg.png');
-    }
-
     create() {
         this.scene.start('Preloader');
     }
